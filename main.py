e546693973eed8eafcb82a9434c2a00befb0f9c4
bipsonthapa <bipsonthapa@mail.weber.edu>
2025-04-24 14:30:26 -0600

    ADDED: dog instance and specific sound

--- main.py
+++ main.py
@@ -15,13 +15,15 @@ if __name__ == "__main__":
 
 # TODO: Create an instance of the Dog class
     dog1 = Dog("Buddy", "Canine", "Golden Retriever")
-# TODO: Print the Dog instance
 
-     print(dog1.speak())
-     print("-----")
-     
+# TODO: Print the Dog instance
     print(dog1)
+
 # TODO: Call the method to make the dog-specific sound
+    print(dog1.speak())
+    print("-----")
+
 # TODO print out all the animals
     print("All animals:")
- 
+    Animal.all_animals
+    print(dog1) 
